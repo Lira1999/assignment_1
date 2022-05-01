15dc08ec2adf97e492974d78d32f66838884a095
Lira1999 <nishatsadaflira@gmail.com>
2022-05-01 16:33:18 +0600

    1st commit

---
+++ java1.java
@@ -0,0 +1,11 @@
+package assignment_1;
+
+/**
+ * java1
+ */
+public class java1 {
+
+  public static void main(String[] args) {
+    System.out.println("java 1");
+  }
+}
---
+++ java2.java
@@ -0,0 +1,8 @@
+package assignment_1;
+
+public class java2 {
+  public static void main(String[] args) {
+    System.out.println("Java 2");
+  }
+  
+}
