8dd10cf99f08de824e09bd219ff24a061edfdaa3
Lira1999 <nishatsadaflira@gmail.com>
2022-05-01 16:53:05 +0600

    after branching 2nd change to main branch

--- java2.java
+++ java2.java
@@ -2,7 +2,7 @@ package assignment_1;
 
 public class java2 {
   public static void main(String[] args) {
-    System.out.println("Java 2");
+    System.out.println("Java 2 after branching");
   }
   
 }
